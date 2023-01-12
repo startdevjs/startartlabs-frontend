b2de60344b11e73e13ace8051f40b008ea397f3f
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-01-12 12:40:41 -0300

    feat: style list topics in community

--- src/pages/community/index.jsx
+++ src/pages/community/index.jsx
@@ -1,13 +1,102 @@
-import {Message, Description} from "./styles"
+import { useEffect } from "react";
+import {
+  Container,
+  TopicCard,
+  CardHeader,
+  CardHeaderDate,
+  ContainerButtons,
+  CardContent,
+  AvatarArea,
+  Avatar,
+  AvatarImgContainer,
+  Author,
+  AuthorName,
+  AuthorUsername,
+  AuthorShield,
+  CardTextContent,
+} from "./styles";
 
 const Community = () => {
-return (
+  return (
     <>
-    <Message>Em breve...</Message>
+      {/* <Message>Em breve...</Message>
         <Description>
             <p>Você fará parte de uma comunidade de programadores!</p>
-        </Description>
-    </>
-)}
+        </Description> */}
 
-export default Community
+      <Container>
+        <TopicCard>
+          <CardHeader>
+            <CardHeaderDate>April 15, 2019 at 4:05 am</CardHeaderDate>
+            <ContainerButtons>
+              <a>Report</a>
+              <a>Responder</a>
+            </ContainerButtons>
+          </CardHeader>
+
+          <CardContent>
+            <AvatarArea>
+              <Avatar>
+                <AvatarImgContainer>
+                  <img src="https://images.unsplash.com/photo-1628157588553-5eeea00af15c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGF2YXRhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
+                </AvatarImgContainer>
+              </Avatar>
+
+              {/* <Avatar>
+                <div class="tile m-0 level">
+                  <div class="tile__icon">
+                    <figure class="avatar avatar--lg">
+                      <img src="https://images.unsplash.com/profile-1495427732560-fe5248ad6638?dpr=1&amp;auto=format&amp;fit=crop&amp;w=256&amp;h=256&amp;q=60&amp;cs=tinysrgb&amp;crop=faces&amp;bg=fff" />
+                    </figure>
+                  </div>
+                </div>
+              </Avatar> */}
+
+              <Author>
+                <AuthorName>John Doe</AuthorName>
+                <AuthorUsername>@luismarchio03</AuthorUsername>
+
+                <AuthorShield
+                // background={}
+                >
+                  Criador
+                </AuthorShield>
+              </Author>
+            </AvatarArea>
+
+            <CardTextContent>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+              dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+              mollit anim id est laborum.
+            </CardTextContent>
+          </CardContent>
+        </TopicCard>
+      </Container>
+
+      <svg
+        height="0"
+        width="0"
+        style={{
+          background: "#40d04f !important",
+        }}
+      >
+        <defs>
+          <clipPath id="svgPath">
+            <path
+              fill="#40d04f"
+              stroke="#000000"
+              stroke-width="1.5794"
+              stroke-miterlimit="10"
+              d="M93.0139721,22.6 L56.6866267,1.8 C54.5908184,0.6 52.1956088,0 49.9001996,0 C47.6047904,0 45.2095808,0.6 43.1137725,1.8 L6.78642715,22.6 C2.59481038,25 0,29.4 0,34.2 L0,75.8 C0,80.6 2.59481038,85 6.78642715,87.4 L43.2135729,108.2 C45.3093812,109.4 47.6047904,110 50,110 C52.2954092,110 54.6906188,109.4 56.7864271,108.2 L93.2135729,87.4 C97.4051896,85 100,80.6 100,75.8 L100,34.2 C99.8003992,29.4 97.2055888,25 93.0139721,22.6 L93.0139721,22.6 Z"
+            />
+          </clipPath>
+        </defs>
+      </svg>
+    </>
+  );
+};
+
+export default Community;
--- src/pages/community/styles.js
+++ src/pages/community/styles.js
@@ -25,3 +25,129 @@ export const Description = styled.p`
     font-size: 1.4rem !important;
   }
 `;
+
+export const Container = styled.div`
+  width: 100%;
+`;
+
+export const TopicCard = styled.div`
+  width: 100%;
+  padding: 2rem;
+`;
+
+export const CardHeader = styled.div`
+  width: 100%;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+
+  background-color: #21283b;
+`;
+
+export const CardHeaderDate = styled.div`
+  color: #9aa4bf;
+  font-size: 0.875rem;
+  font-weight: 500;
+`;
+
+export const ContainerButtons = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+export const CardContent = styled.div`
+  width: 100%;
+
+  display: flex;
+  /* align-items: center; */
+
+  /* gap: 1rem; */
+
+  background-color: #161b28;
+`;
+
+export const AvatarArea = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export const Avatar = styled.div`
+  width: 100px;
+  height: 110px;
+  filter: drop-shadow(0 3px 0 rgba(0, 0, 0, 0.1));
+
+  margin-top: 1rem;
+`;
+
+export const AvatarImgContainer = styled.div`
+  -webkit-clip-path: url(#svgPath);
+  clip-path: url(#svgPath);
+  position: relative;
+
+  &:before {
+    display: block;
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: url("data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9Im5vIj8+PHN2ZyB3aWR0aD0iMTAwcHgiIGhlaWdodD0iMTEwcHgiIHZpZXdCb3g9IjAgMCAxMDAgMTEwIiB2ZXJzaW9uPSIxLjEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiPjxnIHN0cm9rZT0iI0ZGRkZGRiIgc3Ryb2tlLXdpZHRoPSI1IiBmaWxsPSJub25lIj48cGF0aCBkPSJNOTMuMDEzOTcyMSwyMi42IEw1Ni42ODY2MjY3LDEuOCBDNTQuNTkwODE4NCwwLjYgNTIuMTk1NjA4OCwwIDQ5LjkwMDE5OTYsMCBDNDcuNjA0NzkwNCwwIDQ1LjIwOTU4MDgsMC42IDQzLjExMzc3MjUsMS44IEw2Ljc4NjQyNzE1LDIyLjYgQzIuNTk0ODEwMzgsMjUgMCwyOS40IDAsMzQuMiBMMCw3NS44IEMwLDgwLjYgMi41OTQ4MTAzOCw4NSA2Ljc4NjQyNzE1LDg3LjQgTDQzLjIxMzU3MjksMTA4LjIgQzQ1LjMwOTM4MTIsMTA5LjQgNDcuNjA0NzkwNCwxMTAgNTAsMTEwIEM1Mi4yOTU0MDkyLDExMCA1NC42OTA2MTg4LDEwOS40IDU2Ljc4NjQyNzEsMTA4LjIgTDkzLjIxMzU3MjksODcuNCBDOTcuNDA1MTg5Niw4NSAxMDAsODAuNiAxMDAsNzUuOCBMMTAwLDM0LjIgQzk5LjgwMDM5OTIsMjkuNCA5Ny4yMDU1ODg4LDI1IDkzLjAxMzk3MjEsMjIuNiBMOTMuMDEzOTcyMSwyMi42IFoiPjwvcGF0aD48L2c+PC9zdmc+")
+      center center no-repeat;
+    background-size: 100px 107px;
+  }
+
+  img {
+    display: block;
+    width: 110px;
+    height: 110px;
+    margin-left: -5px;
+  }
+`;
+
+export const Author = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const AuthorName = styled.div`
+  color: #fff;
+  font-weight: 700;
+`;
+
+export const AuthorUsername = styled.div`
+  color: #9aa4bf;
+  font-weight: 500;
+
+  &:hover {
+    color: #4ff461;
+    font-weight: 600;
+  }
+`;
+
+export const AuthorShield = styled.div`
+  height: 20px;
+  margin-top: 12px;
+  padding: 0 8px;
+  border-radius: 200px;
+  /* background-color: #7750f8; */
+  background-color: #40d04f;
+  color: #fff;
+  font-size: 0.75rem;
+  font-weight: 700;
+  line-height: 20px;
+  text-transform: uppercase;
+`;
+
+export const CardTextContent = styled.div``;
